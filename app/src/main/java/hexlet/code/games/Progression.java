3aed998cfda33e1e8a0959d1cce61a0cb08d1cd2
jmih3577 <91685545+jmih3577@users.noreply.github.com>
2024-02-15 23:51:37 +0300

    Update Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,7 +4,7 @@ import hexlet.code.Engine;
 
 import java.util.Scanner;
 import static hexlet.code.Engine.getRandomNumberUsingNextInt;
-import static hexlet.code.Engine.winGamesCount;
+import static hexlet.code.Engine.WIN_GAMES_COUNT;
 
 public class Progression {
     private static final int MIN_RANGE_RANDOM = 0;
@@ -18,9 +18,9 @@ public class Progression {
         int progressionQuestionNumber = 0;
         String question;
         String answer = null;
-        String[][] answers = new String[winGamesCount][2];
+        String[][] answers = new String[WIN_GAMES_COUNT][2];
         System.out.println("What number is missing in the progression?");
-        for (int i = 0; i < winGamesCount; i++) {
+        for (int i = 0; i < WIN_GAMES_COUNT; i++) {
             question = "";
             progressionInitNumber = getRandomNumberUsingNextInt(MIN_RANGE_RANDOM, MAX_RANGE_RANDOM);
             progressionIterationNumber = getRandomNumberUsingNextInt(MIN_RANGE_RANDOM, MAX_RANGE_RANDOM);
