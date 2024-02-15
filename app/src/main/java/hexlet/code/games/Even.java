98e637b207ffa5c6bb02c286a9b8a68a5861fa91
jmih3577 <91685545+jmih3577@users.noreply.github.com>
2024-02-15 21:43:03 +0300

    Update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -7,12 +7,14 @@ import static hexlet.code.Engine.getRandomNumberUsingNextInt;
 import static hexlet.code.Engine.winGamesCount;
 
 public class Even {
+    private static final int MIN_RANGE_RANDOM = 0;
+    private static final int MAX_RANGE_RANDOM = 100;
     public static void play(Scanner scan) {
         int evenNumber = 0;
         String[][] answers = new String[winGamesCount][2];
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         for (int i = 0; i < winGamesCount; i++) {
-            evenNumber = getRandomNumberUsingNextInt(0, 100);
+            evenNumber = getRandomNumberUsingNextInt(MIN_RANGE_RANDOM, MAX_RANGE_RANDOM);
             answers[i][0] = "Question: " + evenNumber;
             if (evenNumber % 2 == 0) {
                 answers[i][1] = "yes";
